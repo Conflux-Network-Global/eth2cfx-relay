d8f46aec46b3ce54a5e0e059c18fc20adb2f8a73
aalu1418 <aalu1418@gmail.com>
2020-05-13 15:56:20 -0400

    replaced conflux-web package with js-conflux-sdk b/c deprecation

--- relay/test.js
+++ relay/test.js
@@ -1,31 +1,20 @@
-'use strict';
+"use strict";
 
-// const jayson = require('jayson');
-//
-// // create a client
-// const client = jayson.client.http({
-//   port: 3000
-// });
-//
-// // invoke "add"
-// client.request('add', [1, 1], function(err, response) {
-//   if(err) throw err;
-//   console.log(response.result); // 2
-// });
+const { Conflux, util } = require("js-conflux-sdk");
 
+const cfx = new Conflux({
+  // url: 'http://testnet-jsonrpc.conflux-chain.org:12537',
+  url: "http://localhost:3000",
+  defaultGasPrice: 100, // The default gas price of your following transactions
+  defaultGas: 1000000, // The default gas of your following transactions
+  logger: console,
+});
 
-const ConfluxWeb = require("conflux-web");
-// const confluxWeb = new ConfluxWeb(
-//   "http://testnet-jsonrpc.conflux-chain.org:12537"
-// );
-// const confluxWeb = new ConfluxWeb(
-//   "http://mainnet-jsonrpc.conflux-chain.org:12537"
-// );
-const confluxWeb = new ConfluxWeb(
-  "http://localhost:3000"
-);
+const test = async () => {
+  let balance = await cfx.getBalance("0x11135d2fcd194785bceb223ad18a45fd66d27a7e");
+  console.log(balance.toString(10));
+  let epochNum = await cfx.getEpochNumber();
+  console.log(epochNum);
+}
 
-// confluxWeb.cfx.getEpochNumber().then(console.log);
-confluxWeb.cfx
-  .getBalance("0x11135d2fcd194785bceb223ad18a45fd66d27a7e")
-  .then(console.log);
+test();
