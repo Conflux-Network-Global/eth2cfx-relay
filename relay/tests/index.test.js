a03518dd1aee4e7b2878edec605d6f67834636a2
aalu1418 <aalu1418@gmail.com>
2020-05-14 16:31:14 -0400

    reorganized + added additional test case for querying fc contract using partial ABI

--- relay/tests/index.test.js
+++ relay/tests/index.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { Conflux } = require("js-conflux-sdk");
+const { abi } = require("./fcPartialABI.js");
 
 //direct connection to conflux network
 const cfx = new Conflux({
@@ -18,11 +19,14 @@ const relay = new Conflux({
   logger: console,
 });
 
-const userAddress = "0x11135d2fcd194785bceb223ad18a45fd66d27a7e";
+const user_address = "0x11135d2fcd194785bceb223ad18a45fd66d27a7e";
+const fc_address = "0x88a8f9b1835ae66b6f1da3c930b7d11220bebf78";
+const cfx_contract = cfx.Contract({ abi, address: fc_address });
+const relay_contract = relay.Contract({ abi, address: fc_address });
 
 test("getBalance functions correctly for relay", async () => {
-  const cfx_balance = await cfx.getBalance(userAddress);
-  const relay_balance = await relay.getBalance(userAddress);
+  const cfx_balance = await cfx.getBalance(user_address);
+  const relay_balance = await relay.getBalance(user_address);
   expect(cfx_balance.toString(10)).toBe(relay_balance.toString(10));
 });
 
@@ -31,3 +35,11 @@ test("getEpochNumber functions correctly for relay", async () => {
   const relay_epoch = await relay.getEpochNumber();
   expect(cfx_epoch).toBeLessThanOrEqual(relay_epoch); //because blocks are mined so quickly that the first call should have a earlier epoch than the next call
 });
+
+test("calling FC contract functions correctly for relay", async () => {
+  const relay_balance = await relay_contract.totalSupply();
+  const cfx_balance = await cfx_contract.totalSupply();
+  expect(relay_balance.toString()).toBe(cfx_balance.toString());
+});
+
+//issue with test cases is that the endpoint is not very fast so the test cases may timeout
