f2c3acc08f0886e86cc4d3edf0dbe5d9c1c59a14
aalu1418 <aalu1418@gmail.com>
2020-05-15 15:52:42 -0400

    handled CORS errors + passes 3 test cases using web3.js to interface with CFX endpoint

--- relay/index.js
+++ relay/index.js
@@ -1,23 +1,77 @@
 "use strict";
 
 const jayson = require("jayson");
+const cors = require("cors");
+const connect = require("connect");
+const jsonParser = require("body-parser").json;
+const app = connect();
+
+const eth2cfx = {
+  eth_gasPrice: "cfx_gasPrice",
+  eth_blockNumber: "cfx_epochNumber",
+  eth_getBalance: "cfx_getBalance",
+  eth_getStorageAt: "cfx_getStorageAt",
+  eth_getTransactionCount: "cfx_getNextNonce",
+  eth_getCode: "cfx_getCode",
+  eth_sendRawTransaction: "cfx_sendRawTransaction",
+  eth_call: "cfx_call",
+  eth_estimateGas: "cfx_estimateGasAndCollateral",
+  eth_getBlockByHash: "cfx_getBlockByHash",
+  eth_getBlockByNumber: "cfx_getBlockByEpochNumber",
+  eth_getTransactionByHash: "cfx_getTransactionByHash",
+  eth_getTransactionReceipt: "cfx_getTransactionReceipt",
+  eth_getLogs: "cfx_getLogs",
+};
+
+//note: some response/inputs structures are different...not sure how this will affect things yet
+// getStorageAt has a different variable type for position
+// estimateGas, blockByHash, blockByNumber have different return parameters
+const methodFilter = (method) => eth2cfx[method];
+const epochFilter = (params) => {
+  if (params.length > 0) {
+    const lastEntry = params[params.length - 1];
+    if (lastEntry === "latest" || lastEntry === "pending") {
+      params[params.length - 1] = "latest_state";
+    }
+  }
+  return params;
+};
+
+const client = jayson.client.http(
+  "http://mainnet-jsonrpc.conflux-chain.org:12537"
+);
 
 const methods = {
-  //single method where information is passed directly to the endpoint
-  cfx: jayson.client.http(
-    "http://mainnet-jsonrpc.conflux-chain.org:12537"
-  ),
+  //unknown method called (no corresponding method)
+  no_method: function (args, callback) {
+    var error = this.error(-32601); // returns an error with the default properties set
+    callback(error);
+  },
 };
 
 //using a router, all calls can be routed to the method rather than needing unique methods for each call
 const router = {
-  router: function(method, params) {
-    console.log(method, params);
-    return this._methods["cfx"]
-  }
-}
+  router: function (method, params) {
+    method = methodFilter(method);
+    params = epochFilter(params);
+
+    return !method
+      ? this._methods["no_method"]
+      : new jayson.Method((args, callback) => {
+          client.request(method, params, (err, response) => {
+            console.log(method, params, err, response);
+            err ? callback(err) : callback(err, response.result);
+          });
+        });
+  },
+};
 
 // create a server
 const server = jayson.server(methods, router);
 
-server.http().listen(3000);
+app.use(cors());
+app.use(jsonParser());
+app.use(server.middleware());
+
+// server.http().listen(3000);
+app.listen(3000);
--- relay/tests/index.test.js
+++ relay/tests/index.test.js
@@ -2,42 +2,39 @@
 
 const { Conflux } = require("js-conflux-sdk");
 const { abi } = require("./fcPartialABI.js");
+const Web3 = require("web3");
 
 //direct connection to conflux network
 const cfx = new Conflux({
   url: "http://mainnet-jsonrpc.conflux-chain.org:12537",
+  // url: "http://localhost:3000",
   defaultGasPrice: 100, // The default gas price of your following transactions
   defaultGas: 1000000, // The default gas of your following transactions
   logger: console,
 });
 
-//new instance to connect through relay
-const relay = new Conflux({
-  url: "http://localhost:3000",
-  defaultGasPrice: 100, // The default gas price of your following transactions
-  defaultGas: 1000000, // The default gas of your following transactions
-  logger: console,
-});
+//instance to connect through relay (ETH calls)
+const relay = new Web3("http://localhost:3000")
 
 const user_address = "0x11135d2fcd194785bceb223ad18a45fd66d27a7e";
 const fc_address = "0x88a8f9b1835ae66b6f1da3c930b7d11220bebf78";
 const cfx_contract = cfx.Contract({ abi, address: fc_address });
-const relay_contract = relay.Contract({ abi, address: fc_address });
+const relay_contract = new relay.eth.Contract(abi, fc_address);
 
 test("getBalance functions correctly for relay", async () => {
   const cfx_balance = await cfx.getBalance(user_address);
-  const relay_balance = await relay.getBalance(user_address);
-  expect(cfx_balance.toString(10)).toBe(relay_balance.toString(10));
+  const relay_balance = await relay.eth.getBalance(user_address);
+  expect(cfx_balance.toString()).toBe(relay_balance.toString());
 });
 
-test("getEpochNumber functions correctly for relay", async () => {
+test("getBlockNumber functions correctly for relay", async () => {
   const cfx_epoch = await cfx.getEpochNumber();
-  const relay_epoch = await relay.getEpochNumber();
+  const relay_epoch = await relay.eth.getBlockNumber();
   expect(cfx_epoch).toBeLessThanOrEqual(relay_epoch); //because blocks are mined so quickly that the first call should have a earlier epoch than the next call
 });
 
 test("calling FC contract functions correctly for relay", async () => {
-  const relay_balance = await relay_contract.totalSupply();
+  const relay_balance = await relay_contract.methods.totalSupply().call();
   const cfx_balance = await cfx_contract.totalSupply();
   expect(relay_balance.toString()).toBe(cfx_balance.toString());
 });
